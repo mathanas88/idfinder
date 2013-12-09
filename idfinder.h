c43dd1ad9535414ab97bfcf4ff4f1e53bd711edb
Michael Athanas <mathanas@vt.edu>
2013-12-09 10:40:39 -0500

    Renaming idfinder

---
+++ idfinder.h
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////////////////////
+// ECE 2524 Identity Finder Project Header File
+// mathanas, andrew4
+// 12/9/2013
+
+
+#include <iostream>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
